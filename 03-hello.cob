21812900569b1e7a202ba6638e0d5bb1427a9e2f
eddie <eddie@5xruby.tw>
2021-09-20 10:22:34 +0800

    Episode 3 Hello World

---
+++ 03-hello.cob
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Loveyou.
+       AUTHOR. YesICan.
+       DATE-WRITTEN. 2021-09-18.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Hello World I Still Love You!".
---
+++ README.md
@@ -0,0 +1,7 @@
+# COBOL Practice
+
+Learn COBOL programming language just for fun!
+
+For more information, please check out my [Youtube Channel](https://www.youtube.com/channel/UClrlt5TMige_A_HKqpXd8GA)
+
+by eddie@5xcampus.com
