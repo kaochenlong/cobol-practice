c2c639538cbce8da3b46e9f2449083e62b4647e3
eddie <eddie@5xruby.tw>
2021-09-21 11:43:25 +0800

    Episode 6 BMI Calculator

--- 06-bmi-calculator.cob
+++ 06-bmi-calculator.cob
@@ -1,19 +1,19 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. BMICalculator.
-       AUTHOR. YesICan.
-       DATE-WRITTEN. 2021-09-20.
+       AUTHOR. BuiBui.
+       DATE-WRITTEN. 2021-09-21.
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 height PIC 999v99.
-       01 weight PIC 999v99.
-       01 bmi PIC 99v99.
+       01 height PIC 999v9.
+       01 weight PIC 999v9.
+       01 BMI PIC 99v99.
 
        PROCEDURE DIVISION.
-           DISPLAY "please input your height (cm): " WITH NO ADVANCING.
+           DISPLAY "please input your height(cm): " WITH NO ADVANCING.
            ACCEPT height.
-           DISPLAY "and your weight (kg): " WITH NO ADVANCING.
+           DISPLAY "also input your weight(kg): " WITH NO ADVANCING.
            ACCEPT weight.
            COMPUTE height = height / 100.
-           COMPUTE bmi ROUNDED = weight / (height * height).
-           DISPLAY "Your BMI is " bmi.
+           COMPUTE BMI ROUNDED = weight / (height  * height).
+           DISPLAY "Your BMI is: " BMI.
